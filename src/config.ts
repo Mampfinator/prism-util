026ac5e8717697e9e8f4f23de9c30df6e283689c
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2024-03-15 21:26:01 +0100

    add ability to import custom roles from Copa

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,15 @@
 import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
     ChannelType,
     ChatInputCommandInteraction,
+    Colors,
+    ComponentType,
+    EmbedBuilder,
     PermissionsBitField,
     SlashCommandBuilder,
+    Snowflake,
 } from "discord.js";
 
 export const CONFIG_COMMAND = {
@@ -25,9 +32,34 @@ export const CONFIG_COMMAND = {
                         .setRequired(false)
                         .addChannelTypes(ChannelType.GuildText),
                 ),
+        )
+        .addSubcommand(copaImport =>
+            copaImport
+                .setName("copa-import")
+                .setDescription("Import custom roles from Copa.")
+                .addRoleOption(anchor =>
+                    anchor
+                        .setName("anchor")
+                        .setDescription(
+                            "The anchor role as configured in Copa.",
+                        )
+                        .setRequired(true),
+                )
+                .addRoleOption(stopBefore =>
+                    stopBefore
+                        .setName("stop-before")
+                        .setDescription(
+                            "The role after the last custom role. Ignore if the last role in the menu is a custom role.",
+                        )
+                        .setRequired(false),
+                ),
         ) as SlashCommandBuilder,
     execute: async (interaction: ChatInputCommandInteraction) => {
-        if (interaction.options.getSubcommand(false) == "request-channel") {
+        const subcommand = interaction.options.getSubcommand(true) as
+            | "request-channel"
+            | "copa-import";
+
+        if (subcommand == "request-channel") {
             const channel = interaction.options.getChannel("channel", false);
 
             if (!channel) {
@@ -59,6 +91,142 @@ export const CONFIG_COMMAND = {
                 content: `Requests will now be sent to <#${channelId}>`,
                 ephemeral: true,
             });
+        } else if (subcommand == "copa-import") {
+            const anchor = interaction.options.getRole("anchor", true);
+            const stopBefore = interaction.options.getRole(
+                "stop-before",
+                false,
+            );
+
+            const ignored = new Set();
+            const migrate = new Map();
+
+            for (const role of await interaction
+                .guild!.roles.fetch()
+                .then(roles => roles.values())) {
+                if (
+                    role.position < anchor.position ||
+                    (stopBefore && role.position >= stopBefore.position)
+                ) {
+                    continue;
+                }
+
+                // custom roles are supposed to have *exactly* 1 member.
+                if (role.members.size != 1) {
+                    ignored.add(role.id);
+                    continue;
+                }
+
+                const member = role.members.first()!;
+
+                migrate.set(role.id, member.id);
+            }
+
+            const reply = await interaction.reply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor(Colors.Blue)
+                        .setDescription("Applying the following changes:")
+                        .addFields(
+                            {
+                                name: "Ignoring roles",
+                                value:
+                                    ignored.size > 0
+                                        ? Array.from(ignored)
+                                              .map(id => `<@&${id}>`)
+                                              .join("\n")
+                                        : "None",
+                            },
+                            {
+                                name: "Migrating roles",
+                                value:
+                                    migrate.size > 0
+                                        ? Array.from(migrate)
+                                              .map(
+                                                  ([roleId, memberId]) =>
+                                                      `<@${memberId}>: <@&${roleId}>`,
+                                              )
+                                              .join("\n")
+                                        : "None",
+                            },
+                        ),
+                ],
+                components: [
+                    new ActionRowBuilder<ButtonBuilder>().addComponents(
+                        new ButtonBuilder()
+                            .setStyle(ButtonStyle.Success)
+                            .setLabel("Confirm")
+                            .setCustomId("confirm"),
+                    ),
+                    new ActionRowBuilder<ButtonBuilder>().addComponents(
+                        new ButtonBuilder()
+                            .setStyle(ButtonStyle.Danger)
+                            .setLabel("Cancel")
+                            .setCustomId("cancel"),
+                    ),
+                ],
+                fetchReply: true,
+            });
+
+            const answer = await reply
+                .awaitMessageComponent({
+                    componentType: ComponentType.Button,
+                    time: 10000,
+                })
+                .then(c => c.customId as "confirm" | "cancel");
+
+            await interaction.editReply({ components: [] });
+
+            if (answer == "cancel") {
+                await interaction.followUp({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setColor(Colors.Red)
+                            .setDescription("Cancelled migration"),
+                    ],
+                    ephemeral: true,
+                });
+                return;
+            }
+
+            const failed: [Snowflake, Snowflake][] = [];
+
+            for (const [roleId, memberId] of migrate) {
+                await interaction.client.customRoles
+                    .set(`${interaction.guildId}:${memberId}`, roleId)
+                    .catch(() => failed.push([roleId, memberId]));
+            }
+
+            if (failed.length > 0) {
+                await interaction.followUp({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setColor(Colors.Red)
+                            .setDescription(
+                                `Failed to migrate ${failed.length} roles`,
+                            )
+                            .addFields({
+                                name: "Roles",
+                                value: failed
+                                    .map(
+                                        ([roleId, memberId]) =>
+                                            `<@${memberId}>: <@&${roleId}>`,
+                                    )
+                                    .join("\n"),
+                            }),
+                    ],
+                    ephemeral: true,
+                });
+            } else {
+                await interaction.followUp({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setColor(Colors.Green)
+                            .setDescription("Successfully migrated roles"),
+                    ],
+                    ephemeral: true,
+                });
+            }
         }
     },
 };
